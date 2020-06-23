a83543d70b76cdb4c5193d44e16bc34edd964a5c
MickaelLP <jolymickael67340@gmail.com>
2020-06-23 16:12:32 -0400

    1 file modified

--- Nav.js
+++ Nav.js
@@ -1,3 +1,14 @@
 import React from 'react';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+// Exemple de fonction félchée
+
+const Nav = () => {
+  return (
+    <div> // JSX : C'est du code html dans du code react. 
+      <p>Bonjour</p>
+    </div>
+  )
+} 
+
+export default Nav;
+// export default ({ name }) => <h1>Hello {name}!</h1>; // Ligne du début. 
