ed8dbdf61955647fc754dc4c8fd898b059cb23d5
MickaelLP <jolymickael67340@gmail.com>
2020-06-23 14:13:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Commence par modifier pour voir la magie apparaître :) 
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
