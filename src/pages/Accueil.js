85584331ae4568a7783e2d25770eeb22da79cdd0
MickaelLP <jolymickael67340@gmail.com>
2020-06-27 16:36:21 -0400

    1 file modified, 1 added

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -3,7 +3,19 @@ import Navigation from '../components/Navigation.js';
 
 const Accueil = () => {
   return (
-    <Navigation />
+    <div className="home">
+      <Navigation />
+      <div className="accueilContenu">
+        <div className="contenu">
+          <h1>Mickael Joly</h1>
+          <h2>Développement web, front-end et design</h2>
+          <div>
+          <a href="./media/CV.pdf" target="_blank">Télécharger le CV </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
   );
 };
 
